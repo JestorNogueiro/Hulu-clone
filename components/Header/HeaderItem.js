67a27b57d15d53614d730c56d9cbafcfd8e79917
JestorNogueiro <jestornogueiro789@gmail.com>
2021-07-05 13:58:26 +0530

    displaying header title

---
+++ components/Header/HeaderItem.js
@@ -0,0 +1,12 @@
+function HeaderItem({ title, Icon }) {
+  return (
+    <div className="group flex flex-col items-center cursor-pointer w-11 sm:w-20 hover:text-white mt-2">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+        {title}
+      </p>
+    </div>
+  );
+}
+
+export default HeaderItem;
