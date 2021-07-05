cfb25c24e87e6d1c0eba0637359333f56d7edfdd
JestorNogueiro <jestornogueiro789@gmail.com>
2021-07-05 13:53:33 +0530

    Adding Footer

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="h-8 bg-black">
+      <h1 className="flex items-center justify-center opacity-500">
+        @JestorNogueiro
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
